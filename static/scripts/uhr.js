f546aca0fe88e91f8fffe99c3ab18e1617411f55
RichCyph <100386752+RichCyph@users.noreply.github.com>
2022-03-20 16:48:25 +0100

    commiting update

--- static/scripts/uhr.js
+++ static/scripts/uhr.js
@@ -1,47 +1,31 @@
 
-document.getElementById("benutzer_name").innerHTML = global_benutzer_name;
+document.getElementById("benutzer_name").innerText = global_benutzer_name;
 
 var myVar = setInterval(myTimer, 1000);
 function myTimer() {
     var d = new Date();
     var t = d.toLocaleTimeString();
 	var ad = d.toLocaleDateString();
-    $("#uhr").html(t);
-	$("#datum").html(ad);
-	
-	document.getElementById("tageszeit").innerHTML = tageszeit_berechnen(d);
+  document.getElementById("uhr").innerText = t;
+	document.getElementById("datum").innerText = ad;
+
+	document.getElementById("tageszeit").innerText = tageszeit_berechnen(d);
 };
 
 function tageszeit_berechnen(zeit){
 	var stunden = parseInt(zeit.getHours());
 	console.log(stunden);
 	var tageszeit;
-	
+
 	if (stunden >= 5 && stunden < 12) {
-		tageszeit = global_morgengruss;//sprache.morgengruss;
+		tageszeit = global_morgengruss;
 	} else if (stunden >=12 && stunden < 17) {
-		tageszeit = global_nachmittaggruss;//sprache.nachmittaggruss;
+		tageszeit = global_nachmittaggruss;
 	} else if (stunden >= 17 && stunden < 20) {
-		tageszeit = global_abendgruss;//sprache.abendgruss;
+		tageszeit = global_abendgruss;
 	} else {
-		tageszeit = global_nachtgruss;//sprache.nachtgruss;
+		tageszeit = global_nachtgruss;
 	}
-	
-	/*
-	
-	if (stunden >= 5 && stunden < 12) {
-		var mg = {{user.morning_greeting}};
-	tageszeit = mg;
-	} else if (stunden >=12 && stunden < 17) {
-		var mg ={{user.afternoon_greeting}};
-		tageszeit = mg;
-	} else if (stunden >= 17 && stunden < 20) {
-			var mg =  {{user.evening_greeting}};
-		tageszeit = mg;
-	} else {
-		tageszeit = {{user.night_greeting}};
-	}
-	*/
-	
+
 	return tageszeit;
 };
