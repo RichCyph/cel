0698742be00eb11487fd25f377b6679fad7e6736
RichCyph <100386752+RichCyph@users.noreply.github.com>
2022-03-20 16:48:26 +0100

    commiting update

--- static/scripts/global_einstellungen.js
+++ static/scripts/global_einstellungen.js
@@ -1,32 +1,15 @@
-
 //Urh Global Variablen
-var global_benutzer_name = "{{user.name}}";
-
+var global_benutzer_name = document.getElementById("name").innerText; //"{{user.name}}!";
 
 //begrussungen
-var global_morgengruss = "guten morgen";
-var global_nachmittaggruss = "guten nachmittage";
-var global_abendgruss = "guten abend";
-var global_nachtgruss = "gute nacht";
-
+var global_morgengruss = document.getElementById("morning_greeting").innerText; //"{{user.morning_greeting}}";
+var global_nachmittaggruss = document.getElementById("afternoon_greeting").innerText; //"{{user.afternoon_greeting}}";
+var global_abendgruss = document.getElementById("evening_greeting").innerText; //"{{user.evening_greeting}}";
+var global_nachtgruss = document.getElementById("night_greeting").innerText; //"{{user.night_greeting}}";
 
 //Wetter Global Variablen
-var global_lat = 43.767969;
-var global_lon = -79.5017233;
-var global_lande_texte = "Kanada";
+var global_lat = parseInt(document.getElementById("latitude").innerText); //"{{user.latitude}}";
+var global_lon = parseInt(document.getElementById("longitude").innerText); //"{{user.longitude}}";
+var global_lande_texte = document.getElementById("country").innerText; //"{{user.country}}";
 
-
-//Krypto Global Variablen
-var global_query_liste = [["BTC", "CAD"],
-				   ["ETH","CAD"],
-				   ["LINK","CAD"],
-				   ["EOS","CAD"],
-				   ["XLM","CAD"],
-				   ["LTC","CAD"],
-				   ["DOT","CAD"],
-				   ];
-				   
 var global_sprachen_auswahl = "de" //en, de
-
-//wecker_klangen
-
