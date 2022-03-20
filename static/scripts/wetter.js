cd6f1f1858c3cae6ea0c21d641807b45a4a12615
RichCyph <100386752+RichCyph@users.noreply.github.com>
2022-03-20 16:48:28 +0100

    commiting update

--- static/scripts/wetter.js
+++ static/scripts/wetter.js
@@ -3,7 +3,7 @@
 
 var myVar = setInterval(myWetter, 10*60*1000);
 var key = "cb5d35b9624e0f9a70b023db43de964f"
-var latlon = "lat=" + global_lat + "&lon=" + global_lon; //lat lon von global_einstellungen
+var latlon = "lat=" + global_lat + "&lon=" + global_lon; //lat lon von global_einstellungen or hallo.html
 var api_url = "https://api.openweathermap.org/data/2.5/onecall?" + latlon + "&lang=de&units=metric&exclude=minutely&appid=" + key
 
 function myWetter() {
@@ -12,10 +12,10 @@ function myWetter() {
 
     xhttp.onreadystatechange = function() {
          if (this.readyState == 4 && this.status == 200) {
-			 document.getElementById("temp").innerHTML = this.response.current.temp;
-			 document.getElementById("gefuhl").innerHTML = this.response.current.weather[0].main;
+			 document.getElementById("temp").innerText = this.response.current.temp;
+			 document.getElementById("gefuhl").innerText = this.response.current.weather[0].main;
 		 }
-		 document.getElementById("wetter_standort").innerHTML = global_lande_texte;
+		 document.getElementById("wetter_standort").innerText = global_lande_texte;
     };
     xhttp.open("GET", api_url, true);
     xhttp.send();
