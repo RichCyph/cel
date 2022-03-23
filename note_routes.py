b34f9729d5bbcd0da14c46bcd2447ffb2f76c502
RichCyph <100386752+RichCyph@users.noreply.github.com>
2022-03-23 22:40:17 +0100

    commiting update

--- note_routes.py
+++ note_routes.py
@@ -3,7 +3,7 @@
 
 #
 """
-app.py: 
+app.py:
 """
 
 #D DEVELOPMENT
@@ -36,30 +36,30 @@ class Note_Creation_form(FlaskForm):
 	title = StringField('Title', validators=[DataRequired()])
 	text = TextAreaField('Text')
 	create = SubmitField('Create_note')
-	
+
 class Note_Update_form(FlaskForm):
 	title = StringField('Title', validators=[DataRequired()])
 	text = TextAreaField('Text')
-	create = SubmitField('Update_note')	
-	
+	create = SubmitField('Update_note')
+
 @app.route("/create_note", methods=["GET", "POST"])
 def create_note():
 	form = Note_Creation_form()
 	#if request.method == "POST":
 	if form.validate_on_submit():
 		#form = Note_Creation_form()
-		
+
 		title = form.title.data
 		text = form.text.data
 
 		note = Note(title=title, text=text, parent_user=session["user_id"])
-		
+
 		db.session.add(note)
 		db.session.commit()
 		return redirect(url_for('index'))
 	return render_template('notes/create_note.html', form=form)
-	
-	
+
+
 @app.route("/update_note/<int:id>", methods=["GET", "POST"])
 def update_note(id):
 	form = Note_Update_form()
@@ -67,6 +67,9 @@ def update_note(id):
 	if form.validate_on_submit():
 		print('validated!')
 		note = db.session.query(Note).get(id)
+
+		#if(session["user_id"] == None or session["user_id"] != note.parent_user):
+		#	return render_template('login')
 
 		note.title = form.title.data
 		note.text = form.text.data
@@ -77,7 +80,7 @@ def update_note(id):
 	note = db.session.query(Note).get(id)
 	return render_template('notes/update_note.html', form=form,\
 	note_id=id, title=note.title, text=note.text)
-	
+
 @app.route("/delete_note/<int:id>")
 def delete_note(id):
 	#if request.method == "POST":
