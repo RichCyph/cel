b69df7f7b87b33573286f5355cdb375bd91b95d0
RichCyph <100386752+RichCyph@users.noreply.github.com>
2022-03-24 18:00:59 +0100

    commiting update

--- crypto_routes.py
+++ crypto_routes.py
@@ -3,7 +3,7 @@
 
 #
 """
-app.py: 
+app.py:
 """
 
 #D DEVELOPMENT
@@ -38,51 +38,58 @@ class Crypto_Creation_form(FlaskForm):
 	crypto_acronym = StringField('Crypto_acronym', validators=[DataRequired()])
 	fiat_acronym = StringField('Fiat_acronym', validators=[DataRequired()])
 	create = SubmitField('Create_crypto')
-	
+
 class Crypto_Update_form(FlaskForm):
 	title = StringField('Title', validators=[DataRequired()])
 	link = StringField('Link')
 	crypto_acronym = StringField('Crypto_acronym', validators=[DataRequired()])
 	fiat_acronym = StringField('Fiat_acronym', validators=[DataRequired()])
 	create = SubmitField('Create_crypto')
-	
+
 @app.route("/create_crypto", methods=["GET", "POST"])
 def create_crypto():
-	
+	if "user_id" not in session:
+		return redirect(url_for('error_404'))
+
 	form = Crypto_Creation_form()
 	#if request.method == "POST":
 	if form.validate_on_submit():
 		#form = Crypto_Creation_form()
-		
+
 		title = form.title.data
 		link = form.link.data
 		crypto_acronym = form.crypto_acronym.data
 		fiat_acronym = form.fiat_acronym.data
 		parent_user = session["user_id"]
-		
+
 		crypto = Crypto(\
 		title=title,link=link,\
 		crypto_acronym=crypto_acronym,\
 		fiat_acronym=fiat_acronym,\
 		parent_user=parent_user)
-		
+
 		db.session.add(crypto)
 		db.session.commit()
 		return redirect(url_for('index'))
 	return render_template('cryptos/create_crypto.html', form=form)
-	
-	
+
+
 @app.route("/update_crypto/<int:id>", methods=["GET", "POST"])
 def update_crypto(id):
+	if "user_id" not in session:
+		return redirect(url_for('error_404'))
+	elif db.session.query(Crypto).get(id).parent_user != session["user_id"]:
+		return redirect(url_for('error_404'))
+
 	form = Crypto_Update_form()
 	#if request.method == "POST":
-	
+
 	print(form)
 	if form.validate_on_submit():
 		print('validated!')
 		crypto = db.session.query(Crypto).get(id)
 		form = Crypto_Update_form()
-		
+
 		crypto.crypto_acronym = form.crypto_acronym.data
 		crypto.fiat_acronym = form.fiat_acronym.data
 		crypto.title = form.title.data
@@ -95,10 +102,14 @@ def update_crypto(id):
 	crypto = db.session.query(Crypto).get(id)
 	return render_template('cryptos/update_crypto.html', form=form,\
 	crypto_acronym=crypto.crypto_acronym, fiat_acronym=crypto.fiat_acronym, title=crypto.title, link=crypto.link, crypto_id=id)
-	
+
 @app.route("/delete_crypto/<int:id>")
 def delete_crypto(id):
-	#if request.method == "POST":
+	if "user_id" not in session:
+		return redirect(url_for('error_404'))
+	elif db.session.query(Crypto).get(id).parent_user != session["user_id"]:
+		return redirect(url_for('error_404'))
+		
 	crypto = db.session.query(Crypto).get(id)
 	db.session.delete(crypto)
 	db.session.commit()
