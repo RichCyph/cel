785315222c58b13a1a50e6fa4bb599f12d493732
RichCyph <100386752+RichCyph@users.noreply.github.com>
2022-03-24 17:40:58 +0100

    commiting update

--- bookmark_routes.py
+++ bookmark_routes.py
@@ -3,7 +3,7 @@
 
 #
 """
-app.py: 
+app.py:
 """
 
 #D DEVELOPMENT
@@ -38,45 +38,55 @@ class Bookmark_Creation_form(FlaskForm):
 	link = StringField('Link', validators=[DataRequired()])
 	subject_id = IntegerField('Subject_id', validators=[DataRequired()])
 	create = SubmitField('Create_bookmark')
-	
+
 class Bookmark_Update_form(FlaskForm):
 	hotkey = StringField('Hotkey', validators=[DataRequired()])
 	title = StringField('Title', validators=[DataRequired()])
 	link = StringField('Link', validators=[DataRequired()])
 	subject_id = IntegerField('Subject_id', validators=[DataRequired()])
-	create = SubmitField('Update_bookmark')	
-	
+	create = SubmitField('Update_bookmark')
+
 @app.route("/create_bookmark/<int:id>", methods=["GET", "POST"])
 def create_bookmark(id):
-	
+	if "user_id" not in session:
+		redirect(url_for('error_404'))
 	form = Bookmark_Creation_form(subject_id=id)
 	#if request.method == "POST":
 	if form.validate_on_submit():
-		
+
 		hotkey = form.hotkey.data
 		title = form.title.data
 		link = form.link.data
 		subject_id = form.subject_id.data
-		
+
 		bookmark = Bookmark(title=title,hotkey=hotkey,link=link, parent_subject=subject_id, parent_user=session["user_id"])
-		
+
 		db.session.add(bookmark)
 		db.session.commit()
 		return redirect(url_for('index'))
 	session["current_subject"]=id
 	return render_template('bookmarks/create_bookmark.html', form=form, subject_id=id)
-	
-	
+
+
 @app.route("/update_bookmark/<int:subject_id>/<int:bookmark_id>", methods=["GET", "POST"])
 def update_bookmark(subject_id,bookmark_id):
+	bookmark = db.session.query(Bookmark).get(bookmark_id)
+	sbject = db.session.query(Subject).get(subject_id)
+	if "user_id" not in session:
+		redirect(url_for('error_404'))
+	elif bookmark.parent_user != session["user_id"] or subejct.parent_user != session["user_id"]:
+		redirect(url_for('error_404'))
+
+
+
 	form = Bookmark_Update_form()
 	#if request.method == "POST":
-	
+
 	print(form)
 	if form.validate_on_submit():
 		bookmark = db.session.query(Bookmark).get(session["current_bookmark"])
 		form = Bookmark_Update_form()
-		
+
 		bookmark.hotkey = form.hotkey.data
 		bookmark.title = form.title.data
 		bookmark.link = form.link.data
@@ -87,7 +97,7 @@ def update_bookmark(subject_id,bookmark_id):
 	bookmark = db.session.query(Bookmark).get(bookmark_id)
 	return render_template('bookmarks/update_bookmark.html', form=form,\
 	subject_id=subject_id,bookmark_id=bookmark_id, hotkey=bookmark.hotkey, title=bookmark.title, link=bookmark.link)
-	
+
 @app.route("/delete_bookmark/<int:id>")
 def delete_bookmark(id):
 	#if request.method == "POST":
