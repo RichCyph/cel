3ed39e32c5f29ddfcdacbd7effc7fbde27f38801
RichCyph <100386752+RichCyph@users.noreply.github.com>
2022-03-26 16:40:31 +0100

    commiting update

--- static/scripts/modal.js
+++ static/scripts/modal.js
@@ -2,9 +2,21 @@
 
 
 var up = document.getElementById("update_settings");
-up.addEventListener("click", function() {
+up.addEventListener("click", function(e) {
     var modal = document.getElementById("modal_background");
     modal.style.display = "flex";
+    console.log("step 1")
+    $.ajax({
+     url: document.getElementById("update_settings").href,
+     type: "GET",
+     dataType: "json",
+     success: function(data){
+       data = data.data;
+       console.log(data)
+     }
+
+   });
+e.preventDefault();
 });
 
 var cs = document.getElementById("modal_close");
